61f39712c06fe854625e695875b58b5de66ada4f
João Pedro Mafaldo de Paula <jpmp@cin.ufpe.br>
2023-08-19 01:07:37 -0300

    fixing ALUcontroller comments v1

--- projeto_riscv/ALUController.sv
+++ projeto_riscv/ALUController.sv
@@ -10,28 +10,29 @@ module ALUController (
     output logic [3:0] Operation  // operation selection for ALU
 );
 
-  assign Operation[0] = ((ALUOp == 2'b10) && (Funct3 == 3'b110)) ||  // R\I-or
-      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) ||  // R\I->>
-      ((ALUOp == 2'b00) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||
-      ((ALUOp == 2'b10) && (Funct3 == 3'b100) && (Funct7 == 7'b0000000)) ||
+  assign Operation[0] = ((ALUOp == 2'b10) && (Funct3 == 3'b110)) ||  // OR
+      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) || // ADD
+      ((ALUOp == 2'b00) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) || //SRAI
+      ((ALUOp == 2'b10) && (Funct3 == 3'b100) && (Funct7 == 7'b0000000)) || //XOR
       ((ALUOp == 2'b00) && (Funct3 == 3'b010));  // R\I->>>
 
   assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW
-      ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||  // R\I-add
+      ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||  // R\I-add or SUB
       ((ALUOp == 2'b00) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||
-      ((ALUOp == 2'b00) && (Funct3 == 3'b010));  // R\I->>>
+      ((ALUOp == 2'b00) && (Funct3 == 3'b010)) 
+      ((ALUop == 2'b00) && (Funct3 == 3'b000));  // ADDI
 
   assign Operation[2] = ((ALUOp==2'b10) && (Funct3==3'b101) && (Funct7==7'b0000000)) || // R\I->>
       ((ALUOp == 2'b00) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
       ((ALUOp == 2'b00) && (Funct3 == 3'b001)) ||  // R\I-<<
-      ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||  // R\I-<
-      ((ALUOp == 2'b10) && (Funct3 == 3'b110)) ||
-      ((ALUOp == 2'b00) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)); //SUB operation
+      ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||  // SLTI
+      ((ALUOp == 2'b10) && (Funct3 == 3'b110)) ||  //OR
+      ((ALUOp == 2'b00) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)); //SLI
 
   assign Operation[3] = (ALUOp == 2'b01) ||  // BEQ
-      ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||
-      ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000)) ||
-      ((ALUOp == 2'b00) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000));  // R\I-<
+      ((ALUOp == 2'b10) && (Funct3 == 3'b010)) || //SLT
+      ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000)) || //SUB
+      ((ALUOp == 2'b00) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000));  // SLLI
 
 // always_comb begin
 //     $display("ALUOp: %b Funct3: %b Funct7: %b", ALUOp, Funct3, Funct7);
